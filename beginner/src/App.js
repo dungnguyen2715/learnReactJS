9a2de4ef25c3b4965ebad0f0dfbb6dec6ec70994
dungnguyen2701 <bvmldung53@gmail.com>
2025-02-25 17:00:59 +0700

    create h2 with content hello everybody

--- beginner/src/App.js
+++ beginner/src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className='App'>
       <h1>Create by Dung</h1>
+      <h2>Hello everybody</h2>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <p>
